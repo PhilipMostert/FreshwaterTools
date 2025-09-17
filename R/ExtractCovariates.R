c04e0d16ed50ff0228cd7758d91ab4cbe61482a9
PhilipMostert <philip.s.mostert@ntnu.no>
2025-09-17 14:38:51 +0200

    Update ExtractCovariate to work with sf

--- R/ExtractCovariates.R
+++ R/ExtractCovariates.R
@@ -1,7 +1,7 @@
 #' @title extractCovariates
 #' @description Assign covariate information to the observation data and integration points
 #' @param data A \code{metric_graph_data} object.
-#' @param covariates A spatial raster in a format usable by \pkg{terra}.
+#' @param covariates A sf object of covariate data.
 #' @param used Which spatial covariates to include in the model.
 #'
 #' @import MetricGraph
@@ -10,29 +10,21 @@
 
 ExtractCovariates <- function(data, covariates, used = NULL) {
 
-  if (!inherits(data, 'metric_graph_data')) stop ('data needs to be a metric_graph_data object.')
+  if (!inherits(data, 'sf')) stop ('data needs to be a metric_graph_data object.')
 
-  if (!inherits(covariates, 'SpatRaster')) stop ('covariates needs to be a SpatRaster object.')
+  if (!inherits(covariates, 'sf')) stop ('covariates needs to be a sf object.')
 
-  if (is.null(used)) used <- names(covariates)
+  if (is.null(used)) used <- names(covariates)[!names(covariates) %in% attr(covariates, "sf_column")]
   else
     if (!all(used %in% names(covariates))) stop ('Some of the covariates specified in used are not in the raster stack.')
 
-  covs <- terra::extract(terra::subset(covariates, used),  st_sf(data), ID = FALSE)
-  names(covs) <- used
-  if (any(is.na(covs))) {
+  NearFeature <- st_nearest_feature(x = data, y = covariates)
 
-    naRows <- lapply(covs, function(x) which(is.na(x)))
-    naCovs <- names(naRows)[sapply(naRows, length) > 0]
+  dataCovs <- st_drop_geometry(covariates)[NearFeature,]
 
-    for(covName in naCovs){
-      covs[naRows[[cov]], covName] <-
-        PointedSDMs::nearestValue(matrix(st_coordinates(covs[naRows[[covName]],])[,c("X","Y")], ncol = 2), covariates[covName])
+  data <- cbind(data, dataCovs)
 
-    }
+  data
 
-  }
-
-  covs
 
 }
