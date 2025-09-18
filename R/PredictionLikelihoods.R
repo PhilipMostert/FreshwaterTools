c77d09353e289b1848c159c6309385be28c491a7
PhilipMostert <philip.s.mostert@ntnu.no>
2025-09-18 11:20:00 +0200

    RM predictionLikelihoods

--- R/PredictionLikelihoods.R
+++
@@ -1,5 +0,0 @@
-#' @title PredictionLikelihoods
-#' @description Make prediction likelihoods for the multispecies models. The predictions will be made at the integration points.
-#'
-#' @param data A \code{metric_graph_data} object.
-#' @param formulaList A list of the formula's for the predictions.
